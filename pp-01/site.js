4d54c836d67d51c35068b77e966d6f342b37a4bf
coopervk <coopervk@gmail.com>
2018-01-28 19:30:31 -0600

    Add email-checking control structure

--- pp-01/site.js
+++ pp-01/site.js
@@ -2,7 +2,15 @@ $('#contact-form').on('submit',
   function(e) {
     console.log('The form was submitted.');
     var email = $('#email').val();
-    console.log('Your email: ' + email);
-    e.preventDefault();
+    var emailcheck_regex = /.+@.+\..+/g;
+    regex_results = emailcheck_regex.exec(email);
+    console.log(regex_results);
+    if(!regex_results || regex_results.length > 1) {
+      e.preventDefault();
+      /* Yell at user */
+    } else {
+      /* Remove form */
+      /* Thank the user */
+    }
   }
 );
