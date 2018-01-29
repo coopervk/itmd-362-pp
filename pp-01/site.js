40cca6cdc9bdf55d0d80cee0afaa9e55d1cf18f4
coopervk <coopervk@gmail.com>
2018-01-28 20:05:46 -0600

    Change id name in the form check to aavoid hyphens

--- pp-01/site.js
+++ pp-01/site.js
@@ -6,8 +6,8 @@ $('#contact-form').on('submit',
     regex_results = emailcheck_regex.exec(email);
     console.log(regex_results);
     e.preventDefault();
-    if ($("#form-check-results").length == 0) {
-      $("<h1 id='form-check-results'>TEST</h1>").insertAfter($(this));
+    if ($("#form_check_results").length == 0) {
+      $("<h1 id='form_check_results'>TEST</h1>").insertAfter($(this));
     }
     if(!regex_results || regex_results.length > 1) {
       /* Change content of form-check-results to an error message */
